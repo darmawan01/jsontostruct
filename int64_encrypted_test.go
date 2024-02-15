e18a9d1f0cd6911a44783fdb0bc2e7e09795bf48
Darmawan Zulkifli <23370707+darmawan01@users.noreply.github.com>
2024-02-15 23:39:56 +0800

    remove hmmmm

--- int64_encrypted_test.go
+++ int64_encrypted_test.go
@@ -12,7 +12,7 @@ func TestIntEncrypted(t *testing.T) {
 	encryptedValue := "0x02000000E16B019A1A1299744DEA2DD7B7A84A1A5569D8AAF5D5D09C21B7F871F4312D3589A97635FFDB997D297C39C233C14E95"
 	expected := 363301016
 
-	SetPasshrase("P4ss P0lR!&#")
+	SetPasshrase("Test123#$")
 
 	// decoded, err := hex.DecodeString(strings.TrimPrefix(encryptedValue, "0x"))
 	// require.NoError(t, err)
